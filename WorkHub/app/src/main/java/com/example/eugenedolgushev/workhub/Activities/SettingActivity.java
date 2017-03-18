3347c13a72f6bb5f5889f27ead15777c883af7d1
Dolgushev Evgeny <baraxlush1995@gmail.com>
2017-03-18 16:39:28 +0300

    Добавлен экран с настройками

---
+++ WorkHub/app/src/main/java/com/example/eugenedolgushev/workhub/Activities/SettingActivity.java
@@ -0,0 +1,50 @@
+package com.example.eugenedolgushev.workhub.Activities;
+
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.LinearLayout;
+
+import com.example.eugenedolgushev.workhub.R;
+
+public class SettingActivity extends AppCompatActivity {
+
+    private Context m_context;
+    private LinearLayout changePasswordView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_setting);
+        setTitle("Настройки");
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        m_context = this;
+
+        changePasswordView = (LinearLayout) findViewById(R.id.change_password_view);
+        changePasswordView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SettingActivity.this, ChangePasswordActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
