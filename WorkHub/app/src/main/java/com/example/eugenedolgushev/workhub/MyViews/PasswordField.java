8d64c3bd0d32b3716e519d91bb490a2992fa5ec0
Dolgushev Evgeny <baraxlush1995@gmail.com>
2017-03-21 22:29:58 +0300

    Рефакторинг
    Попытка исправить фокус кнопки подтверждения пароля

--- WorkHub/app/src/main/java/com/example/eugenedolgushev/workhub/MyViews/PasswordField.java
+++ WorkHub/app/src/main/java/com/example/eugenedolgushev/workhub/MyViews/PasswordField.java
@@ -15,7 +15,7 @@ public class PasswordField extends android.support.v7.widget.AppCompatEditText {
     private Context m_context;
     private int m_viewID;
     private boolean m_isValidated;
-    private PasswordField m_newPasswordView;
+    private PasswordField m_newPasswordView, m_repeatPasswordView;
     private TextView m_currentPasswordViewError, m_newPasswordViewError, m_repeatPasswordViewError;
     private Button m_confirmBtn;
 
@@ -29,20 +29,31 @@ public class PasswordField extends android.support.v7.widget.AppCompatEditText {
         m_viewID = ID;
     }
 
-    public void addLinks(TextView view) {
+    public void addLinks(TextView view, Button confirmBtn) {
         switch (m_viewID) {
             case R.id.current_password_view:
                 m_currentPasswordViewError = view;
+                m_confirmBtn = confirmBtn;
                 break;
             case R.id.new_password_view:
                 m_newPasswordViewError = view;
+                m_confirmBtn = confirmBtn;
                 break;
         }
     }
 
     public void addLinks(TextView view, PasswordField newPassword, Button confirmBtn) {
-        m_repeatPasswordViewError = view;
-        m_newPasswordView = newPassword;
+        switch (m_viewID) {
+            case R.id.new_password_view:
+                m_newPasswordViewError = view;
+                m_repeatPasswordView = newPassword;
+                break;
+            case R.id.repeat_password_view:
+                m_repeatPasswordViewError = view;
+                m_newPasswordView = newPassword;
+                break;
+        }
+
         m_confirmBtn = confirmBtn;
     }
 
@@ -62,14 +73,37 @@ public class PasswordField extends android.support.v7.widget.AppCompatEditText {
         }
     }
 
+    private void validateCurrentPassword() {
+        String currentPassword = getStringFromSharedPreferences("password", m_context);
+        String inputCurrentPassword = this.getText().toString().trim();
+        if (!currentPassword.equals(inputCurrentPassword)) {
+            m_currentPasswordViewError.setVisibility(View.VISIBLE);
+            setConfirmButtonEnable(false);
+        } else {
+            m_currentPasswordViewError.setVisibility(View.GONE);
+            m_isValidated = true;
+            setConfirmButtonEnable(true);
+        }
+    }
+
     private void validateNewPassword() {
         String newPassword = this.getText().toString().trim();
         if (newPassword.length() < 6) {
             m_newPasswordViewError.setText("Ваш пароль должен одержать не менее 6 знаков.");
             m_newPasswordViewError.setVisibility(View.VISIBLE);
+            setConfirmButtonEnable(false);
+            return;
+        }
+
+        String repeatedPassword = m_repeatPasswordView.getText().toString().trim();
+        if (!repeatedPassword.equals(newPassword)) {
+            m_newPasswordViewError.setText("Пароли не совпадают.");
+            m_newPasswordViewError.setVisibility(View.VISIBLE);
+            setConfirmButtonEnable(false);
         } else {
             m_newPasswordViewError.setVisibility(View.GONE);
             m_isValidated = true;
+            setConfirmButtonEnable(true);
         }
     }
 
@@ -78,6 +112,7 @@ public class PasswordField extends android.support.v7.widget.AppCompatEditText {
         if (repeatedPassword.length() < 6) {
             m_repeatPasswordViewError.setText("Ваш пароль должен одержать не менее 6 знаков.");
             m_repeatPasswordViewError.setVisibility(VISIBLE);
+            setConfirmButtonEnable(false);
             return;
         }
 
@@ -85,26 +120,20 @@ public class PasswordField extends android.support.v7.widget.AppCompatEditText {
         if (!repeatedPassword.equals(newPassword)) {
             m_repeatPasswordViewError.setText("Пароли не совпадают.");
             m_repeatPasswordViewError.setVisibility(View.VISIBLE);
+            setConfirmButtonEnable(false);
         } else {
             m_repeatPasswordViewError.setVisibility(View.GONE);
             m_isValidated = true;
+            setConfirmButtonEnable(true);
+        }
+    }
+
+    private void setConfirmButtonEnable(boolean enable) {
+        m_confirmBtn.setEnabled(enable);
+        if (enable) {
             m_confirmBtn.setBackgroundColor(getResources().getColor(R.color.colorPrimary));
-            m_confirmBtn.setEnabled(true);
-        }
-    }
-
-    private void validateCurrentPassword() {
-        String currentPassword = getStringFromSharedPreferences("password", m_context);
-        String inputCurrentPassword = this.getText().toString().trim();
-        if (!currentPassword.equals(inputCurrentPassword)) {
-            m_currentPasswordViewError.setVisibility(View.VISIBLE);
         } else {
-            m_currentPasswordViewError.setVisibility(View.GONE);
-            m_isValidated = true;
+            m_confirmBtn.setBackgroundColor(getResources().getColor(R.color.blocker));
         }
-    }
-
-    public boolean isValidated() {
-        return m_isValidated;
     }
 }
