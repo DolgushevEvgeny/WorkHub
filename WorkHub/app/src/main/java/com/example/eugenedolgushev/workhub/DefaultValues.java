a786dfa989e1571c0e3acfba6a2c06773c19b263
Dolgushev Evgeny <baraxlush1995@gmail.com>
2017-04-18 12:07:57 +0300

    Рефакторинг
    Добавлены константы адресов для запросов

--- WorkHub/app/src/main/java/com/example/eugenedolgushev/workhub/DefaultValues.java
+++ WorkHub/app/src/main/java/com/example/eugenedolgushev/workhub/DefaultValues.java
@@ -5,6 +5,10 @@ public class DefaultValues {
     public static final String SHARED_PREFERENCES_NAME = "MyStorage";
     public static final String MAIN_URL = "http://192.168.0.32:3000/";
     public static final String LOGIN_URL = "login";
+    public static final String GET_OFFICES = "getOffices";
+    public static final String GET_PLANS = "getPlans";
+    public static final String GET_DAYWORKTIME = "getDayWorkTime";
+    public static final String SET_RESERVATION = "setReservation";
     public static final String REMOVE_RESERVATION_URL = "removeReservation";
     public static final String CAN_TAKE_PLACE_URL = "canTakePlace";
     public static final String MY_RESERVATIONS_URL = "MyReservations";
